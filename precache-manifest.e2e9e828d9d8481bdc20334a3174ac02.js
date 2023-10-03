99b896fbb0c74af6b4069120db50d8c91083a390
Reddy <p.reddy8@dxc.com>
2023-10-04 00:56:57 +0530

    Updates

---
+++ precache-manifest.e2e9e828d9d8481bdc20334a3174ac02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "00e2e2ad8dac13ebfe3f",
+    "url": "/MyCV/static/js/main.00e2e2ad.chunk.js"
+  },
+  {
+    "revision": "35aaa751ebde4c94d3cf",
+    "url": "/MyCV/static/js/1.35aaa751.chunk.js"
+  },
+  {
+    "revision": "b1901b0711452b066716",
+    "url": "/MyCV/static/js/runtime~main.b1901b07.js"
+  },
+  {
+    "revision": "3bade23a2f8e905b99db54a477bf955e",
+    "url": "/MyCV/index.html"
+  }
+];
