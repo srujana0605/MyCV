f14347f871e4f97a345128ee8b0bfdb06db58042
Reddy <p.reddy8@dxc.com>
2023-10-04 00:27:47 +0530

    Initial Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Home(){
+    return(
+        <section id="Home">
+          <div class="top-container">
+            <h1>Srujana Kommu</h1>
+            <h6>
+              an <span>enthusiastic </span>techi
+            </h6>
+            <div class="arrow">
+              <a class="nostyle" href="#About">
+                ↯
+              </a>
+            </div>
+          </div>
+        </section>
+    );
+}
+
+export default Home;
